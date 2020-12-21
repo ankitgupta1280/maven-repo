a1a752dc8f39dc4cd3040d2eed6cbfba6cc439e2
Ankit <ankit205123@gmail.com>
2020-12-21 14:58:48 +0530

    add sub

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,8 +5,13 @@ public class Calc {
 		return num1+num2;
 	}
 	
+	public static int subs(int num1,int num2) {
+		return num1-num2;
+	}
+	
 	public static void main(String []args) {
 		System.out.println("Sum is "+(add(10,20)));
+		System.out.println("Difference is "+(subs(10,20)));
 	}
 
 }
