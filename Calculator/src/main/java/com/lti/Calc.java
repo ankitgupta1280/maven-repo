17dbfa303c51f3f85192e4d14a0df1190e4be849
Ankit <ankit205123@gmail.com>
2020-12-21 15:10:13 +0530

    prod

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,10 +8,14 @@ public class Calc {
 	public static int subs(int num1,int num2) {
 		return num1-num2;
 	}
+	public static int prod(int num1,int num2) {
+		return num1*num2;
+	}
 	
 	public static void main(String []args) {
 		System.out.println("Sum is "+(add(10,20)));
 		System.out.println("Difference is "+(subs(10,20)));
+		System.out.println("Product is "+(prod(10,20)));
 	}
 
 }
